db314637e7eb9b366c50c968f5c0048a9402555e
Alex Gutler <alex11jgt@hotmail.com>
2014-10-16 21:41:34 -0300

    criei a classe TextArea

--- index.php
+++ index.php
@@ -15,21 +15,21 @@
     $email = new \AG\Form\Types\Input\InputBasic('email','email', new \AG\Form\Utils\Label('email','Email:'));
     $password = new \AG\Form\Types\Input\InputBasic('password','password', new \AG\Form\Utils\Label('password','Senha:'));
     $cor = new \AG\Form\Types\Input\InputBasic('cor','telefone', new \AG\Form\Utils\Label('cor','Cor:'));
+    $textarea = new \AG\Form\Types\TextArea\TextArea('mensagem',3,new \AG\Form\Utils\Label('mensagem','Mensagem:'));
     $radio1 = new \AG\Form\Types\Input\InputSelections('radio','gender','male','Male');
     $radio2 = new \AG\Form\Types\Input\InputSelections('radio','gender','female','Female');
     $submit = new \AG\Form\Types\Input\InputActions('submit','submit','Enviar');
-    $reset = new \AG\Form\Types\Input\InputActions('reset','reset','Limpar');
     $checkbox = new \AG\Form\Types\Input\InputSelections('checkbox','remeber','remember','Lembrar Credenciais');
 
     $form->addElement($text);
     $form->addElement($email);
     $form->addElement($password);
     $form->addElement($cor);
+    $form->addElement($textarea);
     $form->addElement($radio1);
     $form->addElement($radio2);
     $form->addElement($checkbox);
     $form->addElement($submit);
-    $form->addElement($reset);
 
     $form->render();
 ?>
