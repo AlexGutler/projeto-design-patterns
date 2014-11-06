65959f98a1e036a4c0cf116ad54d2209d61d8b9e
Alex Gutler <alex11jgt@hotmail.com>
2014-11-06 20:00:05 -0200

    fiz os ajustes

--- fixtures/config.php
+++ fixtures/config.php
@@ -2,10 +2,6 @@
 
 return array(
     'db' => array(
-<<<<<<< HEAD
         'dbname'=>'fixtures/categorias.db'
-=======
-        'dbname'=>'categorias.db'
->>>>>>> e588e6dcb0c45745cf8fad3663f1fa9cad473137
     )
 );
